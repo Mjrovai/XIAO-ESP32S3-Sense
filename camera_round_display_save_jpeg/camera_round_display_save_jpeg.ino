b4f5a021f065c65ff2a662fa8b4ee7042a8e1a5b
Marcelo Rovai <Mjrovai@gmail.com>
2023-06-01 07:44:53 -0400

    Add files via upload

---
+++ camera_round_display_save_jpeg/camera_round_display_save_jpeg.ino
@@ -0,0 +1,206 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#include <SPI.h>
+#include "esp_camera.h"
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
+#define TOUCH_INT D7
+
+/* 
+ * NOTE: Since the XIAO EPS32S3 Sense is designed with three pull-up resistors 
+ * R4~R6 connected to the SD card slot, and the round display also has 
+ * pull-up resistors, the Round Display SD card cannot be read when both 
+ * are used at the same time. To solve this problem, we need to cut off 
+ * J3 on the XIAO ESP32S3 Sense expansion board.
+ * https://wiki.seeedstudio.com/xiao_esp32s3_camera_usage/#preliminary-preparation
+ */
+
+#define SD_CS_PIN 21 // ESP32S3 Sense SD Card Reader
+//#define SD_CS_PIN D2 // XIAO Round Display SD Card Reader
+
+#include "camera_pins.h"
+
+// Width and height of round display
+const int camera_width = 240;
+const int camera_height = 240;
+
+// File Counter
+int imageCount = 1;
+bool camera_sign = false;          // Check camera status
+bool sd_sign = false;              // Check sd status
+
+TFT_eSPI tft = TFT_eSPI();
+
+// SD card write file
+void writeFile(fs::FS &fs, const char * path, uint8_t * data, size_t len){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.write(data, len) == len){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+bool display_is_pressed(void)
+{
+    if(digitalRead(TOUCH_INT) != LOW) {
+        delay(3);
+        if(digitalRead(TOUCH_INT) != LOW)
+        return false;
+    }
+    return true;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+//  while(!Serial);
+
+  // Camera pinout
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 10000000; // Reduced XCLK_FREQ_HZ from 20KHz to 10KHz (no EV-VSYNC-OVF message)
+  //config.frame_size = FRAMESIZE_UXGA;
+  //config.frame_size = FRAMESIZE_SVGA;
+  config.frame_size = FRAMESIZE_240X240;
+  //config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  config.pixel_format = PIXFORMAT_RGB565;
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+  
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if(config.pixel_format == PIXFORMAT_JPEG){
+    if(psramFound()){
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  Serial.println("Camera ready");
+  camera_sign = true; // Camera initialization check passes
+
+  // Display initialization
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_WHITE);
+
+  // Initialize SD card - Card Reader under the camera (21)
+  // Change to D2 for using Display Card Reader
+  if(!SD.begin(SD_CS_PIN)){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  // Determine if the type of SD card is available
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  Serial.print("SD Card Type: ");
+  if(cardType == CARD_MMC){
+    Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+    Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+
+  sd_sign = true; // sd initialization check passes
+
+}
+
+void loop() {
+  if( sd_sign && camera_sign){
+
+    // Take a photo
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Failed to get camera frame buffer");
+      return;
+    }
+    
+    if(display_is_pressed()){
+      Serial.println("display is touched");
+      char filename[32];
+      sprintf(filename, "/image%d.jpg", imageCount);
+      
+      // Save photo to file
+      size_t out_len = 0;
+      uint8_t* out_buf = NULL;
+      esp_err_t ret = frame2jpg(fb, 12, &out_buf, &out_len);
+      if (ret == false) {
+        Serial.printf("JPEG conversion failed");
+      } else {
+        // Save photo to file
+        writeFile(SD, filename, out_buf, out_len);
+        Serial.printf("Saved picture: %s\n", filename);
+        imageCount++;
+        free(out_buf);
+      }
+    }
+  
+    //  images
+    uint8_t* buf = fb->buf;
+    uint32_t len = fb->len;
+    tft.startWrite();
+    tft.setAddrWindow(0, 0, camera_width, camera_height);
+    tft.pushColors(buf, len);
+    tft.endWrite();
+      
+    // Release image buffer
+    esp_camera_fb_return(fb);
+
+    delay(10);
+  }
+}
