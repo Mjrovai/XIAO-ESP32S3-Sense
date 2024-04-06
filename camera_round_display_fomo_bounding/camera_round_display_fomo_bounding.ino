aecfea472ede093207c8e02416ae1da235a14bfa
Djair Guilherme <djair.jr@gmail.com>
2024-04-06 10:48:27 -0300

    Update camera_round_display_fomo_bounding.ino

--- camera_round_display_fomo_bounding/camera_round_display_fomo_bounding.ino
+++ camera_round_display_fomo_bounding/camera_round_display_fomo_bounding.ino
@@ -1,39 +1,15 @@
-/*
- * This code was adapted from the MRovai version for Round Display
- * 
- * Important changes:
- *  - It is not necessary to to cut off J3 on the XIAO ESP32S3 Sense expansion board. 
- *  It is possible to use the XIAO's SD Card Reader. For that you need use SD_CS_PIN as 21.
- * 
- * - The camera buffer data should be captured as RGB565 (raw image) with a 240x240 frame size  
- * to be displayed on the round display. This raw image should be converted to jpeg before save 
- * in the SD card. This can be done with the line: 
- * esp_err_t ret = frame2jpg(fb, 12, &out_buf, &out_len);
- * 
- * - The XCLK_FREQ_HZ should be reduced from 20KHz to 10KHz in order to prevent 
- * the message "no EV-VSYNC-OVF message" that appears on Serial Monitor (probably due the time 
- * added for JPEG conversion. 
- *
- * - This example is mainly to see the Fomo bounding boxes in real  time, using Display.
- *
- * 
- * Adapted by Djair Guilherme, April 2024
- * 
- * 
-*/
-
-// Include TFT Libraries. Don't forget to set the correct setup file for TFT_eSPI
-
+/* Adding TFT Support Libraries */
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
 
-// Your FOMO Inferencing header comes here
-#include <XIAO-ESP32S3-Sense-Small-Object_Detection_inferencing.h>
+/* Adding Edge Impulse Inferencing Library */
+#include <Add_Your_inferencing_here.h> // Remember to change this line!
 #include "edge-impulse-sdk/dsp/image/image.hpp"
 
-// Prepare Camera Pinout
+/* Adding ESP32S3 Sense Camera Support */
 #include "esp_camera.h"
+
 #define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
 
 #define PWDN_GPIO_NUM     -1
@@ -41,7 +17,6 @@
 #define XCLK_GPIO_NUM     10
 #define SIOD_GPIO_NUM     40
 #define SIOC_GPIO_NUM     39
-
 #define Y9_GPIO_NUM       48
 #define Y8_GPIO_NUM       11
 #define Y7_GPIO_NUM       12
@@ -54,108 +29,108 @@
 #define HREF_GPIO_NUM     47
 #define PCLK_GPIO_NUM     13
 
+#define TOUCH_INT D7
+
+/* Original EDGE IMPULSE example parameters 
+*  Change Camera Capture to display same size as TFT 
+*/
 #define EI_CAMERA_RAW_FRAME_BUFFER_COLS           240
 #define EI_CAMERA_RAW_FRAME_BUFFER_ROWS           240
 #define EI_CAMERA_FRAME_BYTE_SIZE                 3
-#define TOUCH_INT D7
 
+/* Private variables */
 static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
 static bool is_initialised = false;
 uint8_t *snapshot_buf; //points to the output of the capture
 
-TFT_eSPI tft = TFT_eSPI();
+/* Camera Config Parameters */
+static camera_config_t camera_config = {
+  .pin_pwdn = PWDN_GPIO_NUM,
+  .pin_reset = RESET_GPIO_NUM,
+  .pin_xclk = XCLK_GPIO_NUM,
+  .pin_sscb_sda = SIOD_GPIO_NUM,
+  .pin_sscb_scl = SIOC_GPIO_NUM,
 
-/* This is just a mix for the Edge Impulse Library and MJRovai original example... */
-const int camera_width = EI_CAMERA_RAW_FRAME_BUFFER_COLS;
-const int camera_height = EI_CAMERA_RAW_FRAME_BUFFER_ROWS;
+  .pin_d7 = Y9_GPIO_NUM,
+  .pin_d6 = Y8_GPIO_NUM,
+  .pin_d5 = Y7_GPIO_NUM,
+  .pin_d4 = Y6_GPIO_NUM,
+  .pin_d3 = Y5_GPIO_NUM,
+  .pin_d2 = Y4_GPIO_NUM,
+  .pin_d1 = Y3_GPIO_NUM,
+  .pin_d0 = Y2_GPIO_NUM,
+  .pin_vsync = VSYNC_GPIO_NUM,
+  .pin_href = HREF_GPIO_NUM,
+  .pin_pclk = PCLK_GPIO_NUM,
 
-/* Function definitions , from Edge Impulse default generated library */
+  //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+  .xclk_freq_hz = 10000000,
+  .ledc_timer = LEDC_TIMER_0,
+  .ledc_channel = LEDC_CHANNEL_0,
+
+  /* This config pixel format needed to be the same in function ei_camera_capture ()
+                                                        HERE 
+     bool converted = fmt2rgb888(fb->buf, fb->len, PIXFORMAT_RGB565, snapshot_buf);
+
+  */
+  .pixel_format = PIXFORMAT_RGB565,     //YUV422,GRAYSCALE,RGB565,JPEG
+  .frame_size = FRAMESIZE_240X240,      //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+
+  .jpeg_quality = 12, //0-63 lower number means higher quality
+  .fb_count = 2,       //if more than one, i2s runs in continuous mode. Use only with JPEG
+  .fb_location = CAMERA_FB_IN_PSRAM, // Frame Buffer in PSRAM - YES!
+  .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+};
+
+/* Function definitions ------------------------------------------------------- */
+bool ei_camera_init(void);
 void ei_camera_deinit(void);
 bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) ;
 
-void setup() {
+/* Create TFT_SPI object */
+TFT_eSPI tft = TFT_eSPI();
 
+/* Bounding Boxes Coordinates need to be uint32_t in TFTeSPI Library */
+uint32_t x;
+uint32_t y;
+uint32_t w;
+uint32_t h;
+
+/**
+  @brief      Arduino setup function
+*/
+void setup()
+{
+  // put your setup code here, to run once:
   Serial.begin(115200);
-  while (!Serial);
+  //comment out the below line to start inference immediately after upload
+  // while (!Serial);
 
-  // Preparing Seeed Xiao Round Camera pinout
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 10000000; // Reduced XCLK_FREQ_HZ from 20KHz to 10KHz (no EV-VSYNC-OVF message)
-
-  config.frame_size = FRAMESIZE_240X240; // The round display Resolution 
-
-  config.pixel_format = PIXFORMAT_RGB565; // Raw Camera Input without JPEG conversion
-  
-  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-  config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    if (psramFound()) {
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  } else {
-    // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
-  }
-  
-  // Camera init - Edge Impulse library has an separated function for this... Not needed.
-  
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-  Serial.println("Camera ready");
-  is_initialised = true; // Camera initialization check passes
-
-  // Initialize TFT Display with White Background
+  /* Init TFT */
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_WHITE);
 
-  Serial.println("Edge Impulse Inferencing Demo");
+  if (ei_camera_init() == false) {
+    ei_printf("Failed to initialize Camera!\r\n");
+  }
+  else {
+    ei_printf("Camera initialized\r\n");
+  }
 
   ei_printf("\nStarting continious inference in 2 seconds...\n");
   ei_sleep(2000);
 }
 
+/**
+  @brief      Get data and run inferencing
 
+  @param[in]  debug  Get debug info if true
+*/
 void loop()
 {
 
-  // This remains from Edge Impulse Demo
+  // instead of wait_ms, we'll wait on the signal, this allows threads to cancel us...
   if (ei_sleep(5) != EI_IMPULSE_OK) {
     return;
   }
@@ -168,10 +143,15 @@ void loop()
     return;
   }
 
+  /* The signal that will be inferred is this one */
   ei::signal_t signal;
   signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
   signal.get_data = &ei_camera_get_data;
 
+  /*  Makes the call to the ei_camera_capture function, 
+   *  which captures the image to the framebuffer, 
+   *  converts the image to the correct format and resizes it. 
+  */
   if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, snapshot_buf) == false) {
     ei_printf("Failed to capture image\r\n");
     free(snapshot_buf);
@@ -187,31 +167,42 @@ void loop()
     return;
   }
 
-  // print the predictions
-  ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+  // Print the predictions to Serial Debug
+  ei_printf("TFT Example -  Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
             result.timing.dsp, result.timing.classification, result.timing.anomaly);
 
 #if EI_CLASSIFIER_OBJECT_DETECTION == 1
   bool bb_found = result.bounding_boxes[0].value > 0;
+
+
   for (size_t ix = 0; ix < result.bounding_boxes_count; ix++) {
     auto bb = result.bounding_boxes[ix];
-
-    // This will be used at Rectangle Draw
-    uint32_t x = bb.x;
-    uint32_t y = bb.y;
-    uint32_t w = bb.width;
-    uint32_t h = bb.height;
-    
     if (bb.value == 0) {
       continue;
     }
-    ei_printf("    %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\n", bb.label, bb.value, bb.x, bb.y, bb.width, bb.height);
+    /* 
+      commenting this line, just to compare dimensions above
+      ei_printf("   %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\n", bb.label, bb.value, bb.x, bb.y, bb.width, bb.height);
     
-    // Draw Green Rectangle Bounding Box!
-    tft.drawRect (x,y,w,h, TFT_GREEN);
+    */
+    
+    /* Create Coordinates and Size for Bounding Boxes */
+    x = bb.x;
+    y = bb.y;
+    w = bb.width;
+    h = bb.height;
+
+    /* Checking Sizes */ 
+    ei_printf("BB Coord [ x: %u, y: %u, width: %u, height: %u ]\n",  bb.x, bb.y, bb.width, bb.height);
+    ei_printf("TFT Coord [ x: %u, y: %u, width: %u, height: %u ]\n",  x, y, w, h);
+
+    /* Draw Bounding Boxes in Display */
+    tft.drawRect (x, y, w, h, TFT_GREEN);
+    tft.endWrite();
   }
   if (!bb_found) {
-    ei_printf("    No objects found\n");
+    ei_printf("TFT Example -     No objects found\n");
+    tft.endWrite();
   }
 #else
   for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
@@ -221,17 +212,62 @@ void loop()
 #endif
 
 #if EI_CLASSIFIER_HAS_ANOMALY == 1
-  ei_printf("    anomaly score: %.3f\n", result.anomaly);
+  ei_printf("TFT Example -     anomaly score: %.3f\n", result.anomaly);
 #endif
+
 
   free(snapshot_buf);
 
 }
 
+/**
+   @brief   Setup image sensor & start streaming
 
+   @retval  false if initialisation failed
+*/
+bool ei_camera_init(void) {
+
+  if (is_initialised) return true;
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  //initialize the camera
+  esp_err_t err = esp_camera_init(&camera_config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x\n", err);
+    return false;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, 0); // lower the saturation
+  }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#elif defined(CAMERA_MODEL_ESP_EYE)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+  s->set_awb_gain(s, 1);
+#endif
+
+  is_initialised = true;
+  return true;
+}
+
+/**
+   @brief      Stop streaming of sensor data
+*/
 void ei_camera_deinit(void) {
 
-  //deinitialize the camera - Remains from Edge Impulse Demo
+  //deinitialize the camera
   esp_err_t err = esp_camera_deinit();
 
   if (err != ESP_OK)
@@ -245,6 +281,17 @@ void ei_camera_deinit(void) {
 }
 
 
+/**
+   @brief      Capture, rescale and crop image
+
+   @param[in]  img_width     width of output image
+   @param[in]  img_height    height of output image
+   @param[in]  out_buf       pointer to store output image, NULL may be used
+                             if ei_camera_frame_buffer is to be used for capture and resize/cropping.
+
+   @retval     false if not initialised, image captured, rescaled or cropped failed
+
+*/
 bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) {
   bool do_resize = false;
 
@@ -253,23 +300,26 @@ bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf
     return false;
   }
 
-  // Capture to Framebuffer
   camera_fb_t *fb = esp_camera_fb_get();
 
   if (!fb) {
     ei_printf("Camera capture failed\n");
     return false;
-  } else {
-    // Show image at TFT Display - Yeah
-    tft.startWrite();
-    tft.setAddrWindow(0, 0, camera_width, camera_height);
-    tft.pushColors(fb->buf, fb->len);
-    tft.endWrite();
   }
+/*  Write camera captured framebuffer into TFT Display */
+  tft.startWrite();
+  tft.setAddrWindow(0, 0, EI_CAMERA_RAW_FRAME_BUFFER_COLS, EI_CAMERA_RAW_FRAME_BUFFER_ROWS);
+  tft.pushColors(fb->buf, fb->len);
 
-  // Release Camera Framebuffer
+/*  Pay attention to this PIXFORMAT_RGB565 parameter in camera config. Need to be equal or convertion will fail  */
+  bool converted = fmt2rgb888(fb->buf, fb->len, PIXFORMAT_RGB565, snapshot_buf);
+
   esp_camera_fb_return(fb);
 
+  if (!converted) {
+    ei_printf("Conversion failed\n");
+    return false;
+  }
 
   if ((img_width != EI_CAMERA_RAW_FRAME_BUFFER_COLS)
       || (img_height != EI_CAMERA_RAW_FRAME_BUFFER_ROWS)) {
@@ -308,3 +358,6 @@ static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr)
   return 0;
 }
 
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_CAMERA
+#error "Invalid model for current sensor"
+#endif
